ed3d0b4b057144f5634402dd008ec3adeffb8cb3
Claudiano-Flores <claudiano145@gmail.com>
2022-12-10 18:57:04 -0300

    Composição C#

---
+++ Composition/Entities/Department.cs
@@ -0,0 +1,15 @@
+﻿namespace Composition.Entities
+{
+    internal class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+        }
+        public Department(string name)
+        {
+            Name = name;
+        }   
+    }
+}
